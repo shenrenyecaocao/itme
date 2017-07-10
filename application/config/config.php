85c0368870ee9fbdd58abb86c568f4a0edab8792
wangqi <541885781@qq.com>
2017-07-10 21:00:04 +0800

    [CHANGE] 修改项目默认的配置

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.itme.com';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
