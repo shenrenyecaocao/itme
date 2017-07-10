febdcdd8c02546adfd2373b60059b12557b188b4
wangqi <541885781@qq.com>
2017-07-10 22:28:23 +0800

    23

--- application/views/admin/common/top.php
+++ application/views/admin/common/top.php
@@ -9,11 +9,11 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="<?php echo site_url('admin/home') ?>">管理员：<b><?php echo $_SESSION['current_admin']['name'] ?></b>你好！</a>
+          <a class="navbar-brand" href="<?php echo site_url('admin/dashboard') ?>">管理员：<b><?php echo $_SESSION['current_admin']['name'] ?></b>你好！</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li><a href="<?php echo site_url('index/home') ?>" target="_blank">前台首页</a></li>
+            <li><a href="<?php echo site_url('blog/dashboard') ?>" target="_blank">前台首页</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="<?php echo site_url('admin/logout'); ?>">退出</a></li>
