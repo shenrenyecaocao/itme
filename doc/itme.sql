383bbae34a1ba4486533449190c1e018d2642986
wangqi <541885781@qq.com>
2017-07-10 21:42:44 +0800

    [NEW] 增加项目的基础数据库

--- doc/itme.sql
+++ doc/itme.sql
@@ -1,18 +1,82 @@
-CREATE TABLE IF NOT EXISTS `administrators` (
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+--
+-- Host: localhost    Database: itme
+-- ------------------------------------------------------
+-- Server version	5.7.18-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `it_admin`
+--
+
+DROP TABLE IF EXISTS `it_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `it_admin` (
   `admin_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(15) DEFAULT NULL,
   `email` varchar(30) NOT NULL,
   `password` varchar(40) NOT NULL,
   `role_id` int(11) unsigned NOT NULL,
-  `update_date` timestamp NOT NULL,
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `active_status` tinyint(4) NOT NULL DEFAULT '0',
   `del_flg` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0:undelete;1:delete',
+  `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`admin_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `role` (
+--
+-- Dumping data for table `it_admin`
+--
+
+LOCK TABLES `it_admin` WRITE;
+/*!40000 ALTER TABLE `it_admin` DISABLE KEYS */;
+INSERT INTO `it_admin` VALUES (1,'桑榆非晚','wangqi@itme.com','10470c3b4b1fed12c3baac014be15fac67c6e815',0,1,0,'2017-07-10 13:38:54','2017-07-10 13:33:32');
+/*!40000 ALTER TABLE `it_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `it_role`
+--
+
+DROP TABLE IF EXISTS `it_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `it_role` (
   `role_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `power` int(11) NOT NULL COMMENT '越小权限越大',
   PRIMARY KEY (`role_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `it_role`
+--
+
+LOCK TABLES `it_role` WRITE;
+/*!40000 ALTER TABLE `it_role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `it_role` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-07-10 21:41:55
