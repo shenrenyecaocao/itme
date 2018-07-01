eac29645a5dafe92eb03cd8aad7e8a3a614ba03e
shenrenyecaocao <541885781@qq.com>
2018-07-01 09:55:26 +0800

    修改上传文件方法

--- application/models/bll/Bll_tool.php
+++ application/models/bll/Bll_tool.php
@@ -32,7 +32,7 @@ class Bll_tool extends CI_Model
     {
         $config['upload_path']      = './uploads/';
         $config['allowed_types']    = 'gif|jpg|png';
-        $config['max_size']     = 100;
+        $config['max_size']     = 1024;
         $config['max_width']        = 1024;
         $config['max_height']       = 768;
         $this->load->library('upload', $config);
@@ -41,6 +41,8 @@ class Bll_tool extends CI_Model
         if ($this->upload->do_upload($file)) {
             return $this->upload->data('file_name');
         } else {
+            $error_msg = $this->upload->display_errors();
+            $this->session->set_flashdata('upload_error', $error_msg);
             return FALSE;
         }
     }
